620e69e450c03e37b955fda6802fb7fdc18295a3
messense <messense@icloud.com>
2018-01-23 12:35:47 +0800

    Restrict par_iter

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ extern crate fasttext;
 use std::path::Path;
 use rayon::prelude::*;
 use rocket::State;
+use rocket::fairing::AdHoc;
 use rocket_contrib::Json;
 use clap::{App, Arg};
 use fasttext::FastText;
@@ -19,6 +20,8 @@ struct PredictOptions {
     k: Option<i32>,
     threshold: Option<f32>,
 }
+
+struct RocketWorkerCount(u16);
 
 #[inline]
 fn predict_one(model: &FastText, text: &str, k: i32, threshold: f32) -> (Vec<String>, Vec<f32>) {
@@ -33,24 +36,34 @@ fn predict_one(model: &FastText, text: &str, k: i32, threshold: f32) -> (Vec<Str
 }
 
 #[post("/predict", format = "application/json", data = "<texts>")]
-fn predict_without_option(model: State<FastText>, texts: Json<Vec<String>>) -> Json<Vec<(Vec<String>, Vec<f32>)>> {
+fn predict_without_option(worker_count: State<RocketWorkerCount>, model: State<FastText>, texts: Json<Vec<String>>)
+    -> Json<Vec<(Vec<String>, Vec<f32>)>>
+{
     // XXX: https://github.com/SergioBenitez/Rocket/issues/376
-    predict(model, texts, Default::default())
+    predict(worker_count, model, texts, Default::default())
 }
 
 #[post("/predict?<options>", format = "application/json", data = "<texts>")]
-fn predict(model: State<FastText>, texts: Json<Vec<String>>, options: PredictOptions) -> Json<Vec<(Vec<String>, Vec<f32>)>> {
+fn predict(worker_count: State<RocketWorkerCount>, model: State<FastText>, texts: Json<Vec<String>>, options: PredictOptions)
+    -> Json<Vec<(Vec<String>, Vec<f32>)>>
+{
     let k = options.k.unwrap_or(1);
     let threshold = options.threshold.unwrap_or(0.0);
     let text_count = texts.len();
-    let ret: Vec<(Vec<String>, Vec<f32>)> = if text_count > 1{
-        texts.par_iter().map(|txt| {
-            predict_one(model.inner(), txt, k, threshold)
-        }).collect()
-    } else if text_count == 1 {
-        vec![predict_one(model.inner(), &texts[0], k, threshold)]
-    } else {
-        Vec::new()
+    let worker_count = worker_count.inner().0 as usize;
+    let ret: Vec<(Vec<String>, Vec<f32>)> = match text_count {
+        0 => Vec::new(),
+        1 => vec![predict_one(model.inner(), &texts[0], k, threshold)],
+        n if n > 1 && n <= worker_count => {
+            texts.par_iter().map(|txt| {
+                predict_one(model.inner(), txt, k, threshold)
+            }).collect()
+        },
+        _ => {
+            texts.iter().map(|txt| {
+                predict_one(model.inner(), txt, k, threshold)
+            }).collect()
+        }
     };
     Json(ret)
 }
@@ -76,6 +89,10 @@ fn main() {
     fasttext.load_model(model_path);
     rocket::ignite()
         .manage(fasttext)
+        .attach(AdHoc::on_attach(|rocket| {
+            let workers = rocket.config().workers;
+            Ok(rocket.manage(RocketWorkerCount(workers)))
+        }))
         .mount("/", routes![predict, predict_without_option])
         .launch();
 }
